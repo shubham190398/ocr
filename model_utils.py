a76e85c1e7f607af33d62b85fd453a9a079d4723
Shubham Acharya <shubham190398@gmail.com>
2023-06-05 13:56:22 +0530

    Adding the architecture of one residual block for use in creating a model

--- model_utils.py
+++ model_utils.py
@@ -13,3 +13,27 @@ def activation_layer(layer, activation="relu", alpha=0.1):
         layer = layers.LeakyReLU(alpha=alpha)(layer)
 
     return layer
+
+
+def residual_block(x, filter_num, strides=2, kernel_size=3, skip_conv=True, padding="same",
+                   kernel_initializer="he_uniform", activation="leaky_relu", dropout=0.2):
+    skip_x = x
+
+    x = layers.Conv2D(filter_num, kernel_size, padding=padding, kernel_initializer=kernel_initializer)(x)
+    x = layers.BatchNormalization()(x)
+    x = activation_layer(x, activation=activation)
+
+    x = layers.Conv2D(filter_num, kernel_size, padding=padding, kernel_initializer=kernel_initializer)
+    x = layers.BatchNormalization()(x)
+
+    if skip_conv:
+        skip_x = layers.Conv2D(filter_num, 1, padding=padding, strides=strides,
+                               kernel_initializer=kernel_initializer)(skip_x)
+
+    x = layers.add()([x, skip_x])
+    x = activation_layer(x, activation=activation)
+
+    if dropout:
+        x = layers.Dropout(dropout)(x)
+
+    return x
