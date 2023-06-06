67270435481e8fbcc9d06e50fc036d0e451c9a0c
Shubham Acharya <shubham190398@gmail.com>
2023-06-06 12:54:36 +0530

    Adding Label Padding Class

--- transformers.py
+++ transformers.py
@@ -88,3 +88,14 @@ class LabelIndexer(Transformer):
     def __call__(self, data: np.ndarray, label: np.ndarray):
         return data, np.array([self.vocab.index(t) for t in label if t in self.vocab])
 
+
+class LabelPadding(Transformer):
+    def __init__(self, max_word_length: int, padding_value: int) -> None:
+        super().__init__()
+        self.max_word_length = max_word_length
+        self.padding_value = padding_value
+
+    # noinspection PyMethodOverriding
+    def __call__(self, data: np.ndarray, label: np.ndarray):
+        return data, np.pad(label, (0, self.max_word_length - len(label)), "constant",
+                            constant_values=self.padding_value)
