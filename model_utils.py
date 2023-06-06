933fb36781072bdf8cad6187ffbaff3ee5736dd0
Shubham Acharya <shubham190398@gmail.com>
2023-06-06 13:01:43 +0530

    Correcting errors and adding CTCLoss function to model_utils

--- model_utils.py
+++ model_utils.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from keras import layers
 
 
@@ -5,7 +6,7 @@ def activation_layer(layer, activation="relu", alpha=0.1):
     if activation == "relu":
         layer = layers.ReLU()(layer)
     elif activation == "leaky_relu":
-        layer = layers.LeakyReLU()(layer)
+        layer = layers.LeakyReLU(alpha=alpha)(layer)
 
     return layer
 
@@ -18,7 +19,7 @@ def residual_block(x, filter_num, strides=2, kernel_size=3, skip_conv=True, padd
     x = layers.BatchNormalization()(x)
     x = activation_layer(x, activation=activation)
 
-    x = layers.Conv2D(filter_num, kernel_size, padding=padding, kernel_initializer=kernel_initializer)
+    x = layers.Conv2D(filter_num, kernel_size, padding=padding, kernel_initializer=kernel_initializer)(x)
     x = layers.BatchNormalization()(x)
 
     if skip_conv:
@@ -32,3 +33,23 @@ def residual_block(x, filter_num, strides=2, kernel_size=3, skip_conv=True, padd
         x = layers.Dropout(dropout)(x)
 
     return x
+
+
+# noinspection PyAbstractClass
+class CTCloss(tf.keras.losses.Loss):
+    def __init__(self, name: str = "CTCLoss") -> None:
+        super(CTCloss, self).__init__()
+        self.name = name
+        self.loss_fn = tf.keras.backend.ctc_batch_cost
+
+    def __call__(self, y_true: tf.Tensor, y_pred: tf.Tensor, sample_weight=None) -> tf.Tensor:
+        batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+        input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+        label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
+
+        input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+        label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+
+        loss = self.loss_fn(y_true, y_pred, input_length, label_length)
+
+        return loss
