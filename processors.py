609004e8c2a2cf1fc12ed2632e824e05fdb2bced
Shubham Acharya <shubham190398@gmail.com>
2023-06-06 11:11:28 +0530

    Adding processors and dataloaders to Git

---
+++ processors.py
@@ -0,0 +1,8 @@
+import cv2
+import typing
+import numpy as np
+import logging
+
+logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s: %(message)s")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
