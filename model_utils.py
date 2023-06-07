6b4c10b51b8e1d3e299c9898cff976d6b24b63db
Shubham Acharya <shubham190398@gmail.com>
2023-06-07 14:38:54 +0530

    Adding a train logger to model utilities

--- model_utils.py
+++ model_utils.py
@@ -4,6 +4,8 @@ from keras.metrics import Metric
 from keras.callbacks import Callback
 import onnx
 import tf2onnx
+import logging
+import os
 
 
 def activation_layer(layer, activation="relu", alpha=0.1):
@@ -116,3 +118,32 @@ class Model2Onnx(Callback):
                 meta.value = value
 
             onnx.save(onnx_model, self.onnx_model_path)
+
+
+class TrainLogger(Callback):
+    def __init__(self, log_path: str, log_file: str = "logs.log", logLevel=logging.INFO, console_output=False) -> None:
+        super().__init__()
+        self.log_path = log_path
+        self.log_file = log_file
+
+        if not os.path.exists(log_path):
+            os.mkdir(log_path)
+
+        self.logger = logging.getLogger()
+        self.logger.setLevel(logLevel)
+
+        self.formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+        self.file_handler = logging.FileHandler(os.path.join(self.log_path, self.log_file))
+        self.file_handler.setLevel(logLevel)
+        self.file_handler.setFormatter(self.formatter)
+
+        if not console_output:
+            self.logger.handlers[:] = []
+
+        self.logger.addHandler(self.file_handler)
+
+    def on_epoch_end(self, epoch: int, logs: dict = None) -> None:
+        epoch_message = f"Epoch {epoch}; "
+        logs_message = "; ".join([f"{key}: {value}" for key, value in logs.items()])
+        self.logger.info(epoch_message + logs_message)
