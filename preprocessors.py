5b1292eaaf0014be7eec3cfbb6b149e63cbfeb30
Shubham Acharya <shubham190398@gmail.com>
2023-06-07 13:11:56 +0530

    Adding the call definition

--- preprocessors.py
+++ preprocessors.py
@@ -14,3 +14,26 @@ class ImageReader:
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.setLevel(log_level)
         self._image_class = image_class
+
+    def __call__(
+            self,
+            image_path: typing.Union[str, np.ndarray],
+            label: typing.Any
+    ) -> typing.Tuple[NormalImage, typing.Any]:
+        if isinstance(image_path, str):
+            if not os.path.exists(image_path):
+                raise FileNotFoundError(f"Image {image_path} not found.")
+
+        elif isinstance(image_path, np.ndarray):
+            pass
+
+        else:
+            raise TypeError(f"Image {image_path} is not a string or numpy array.")
+
+        image = self._image_class(image=image_path)
+
+        if not image.init_successful:
+            image = None
+            self.logger.warning(f"Image {image_path} could not be read, returning None.")
+
+        return image, label
