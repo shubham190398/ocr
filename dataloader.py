2cebfa93eca3c868653d2b77d62a0e059cde790a
Shubham Acharya <shubham190398@gmail.com>
2023-06-07 12:51:56 +0530

    Refactoring augmentors and transformers for _object consistency

--- dataloader.py
+++ dataloader.py
@@ -29,8 +29,8 @@ class DataLoader:
         self.batch_size = batch_size
         self.shuffle = shuffle
         self._epoch = initial_epoch
-        self.augmentors = [] if augmentors is None else augmentors
-        self.transformers = [] if transformers is None else transformers
+        self._augmentors = [] if augmentors is None else augmentors
+        self._transformers = [] if transformers is None else transformers
         self.skip_validation = skip_validation
         self.limit = limit
         self.use_cache = use_cache
@@ -54,32 +54,32 @@ class DataLoader:
 
     @property
     def augmentors(self) -> typing.List[Augmentor]:
-        return self.augmentors
+        return self._augmentors
 
     @augmentors.setter
     def augmentors(self, augmentors: typing.List[Augmentor]):
         for augmentor in augmentors:
             if isinstance(augmentor, Augmentor):
-                if self.augmentors is not None:
-                    self.augmentors.append(augmentor)
+                if self._augmentors is not None:
+                    self._augmentors.append(augmentor)
                 else:
-                    self.augmentors = [augmentor]
+                    self._augmentors = [augmentor]
 
             else:
                 self.logger.warning(f"Augmentor {augmentor} is not an instance of Augmentor.")
 
     @property
     def transformers(self) -> typing.List[Transformer]:
-        return self.transformers
+        return self._transformers
 
     @transformers.setter
     def transformers(self, transformers: typing.List[Transformer]):
         for transformer in transformers:
             if isinstance(transformer, Transformer):
-                if self.transformers is not None:
-                    self.transformers.append(transformer)
+                if self._transformers is not None:
+                    self._transformers.append(transformer)
                 else:
-                    self.transformers = [transformer]
+                    self._transformers = [transformer]
 
             else:
                 self.logger.warning(f"Transformer {transformer} is not an instance of Transformer.")
@@ -152,3 +152,22 @@ class DataLoader:
         for item in (self[i] for i in range(len(self))):
             yield item
 
+    def process_data(self, batch_data):
+        if self.use_cache and batch_data[0] in self.cache:
+            data, annotation = copy.deepcopy(self.cache[batch_data[0]])
+        else:
+            data, annotation = batch_data
+            for preprocessor in self.data_preprocessors:
+                data, annotation = preprocessor(data, annotation)
+
+            if data is None or annotation is None:
+                self.logger.warning("Data or annotation is None, marking for removal on epoch end.")
+                self.on_epoch_end_remove.append(batch_data)
+                return None, None
+
+            if self.use_cache and batch_data[0] not in self.cache:
+                self.cache[batch_data[0]] = (copy.deepcopy(data), copy.deepcopy(annotation))
+
+        for objects in [self._augmentors, self._transformers]:
+            for _object in objects:
+                data, annotation = _object(data, annotation)
