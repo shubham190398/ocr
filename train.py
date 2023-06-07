b44066d6464359580abb1499be3fe3b609916195
Shubham Acharya <shubham190398@gmail.com>
2023-06-07 15:32:04 +0530

    Cleanup

--- train.py
+++ train.py
@@ -1,13 +1,6 @@
 import os
 from tqdm import tqdm
 import tensorflow as tf
-
-# noinspection PyBroadException
-try:
-    [tf.config.experimental.set_memory_growth(gpu, True) for gpu in tf.config.experimental.list_physical_devices("GPU")]
-except:
-    pass
-
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
 from dataloader import DataLoader
 from preprocessors import ImageReader
@@ -15,3 +8,11 @@ from image_formats import CVImage
 from transformers import ImageResizer, LabelIndexer, LabelPadding
 from model_utils import CTCloss, Model2Onnx, TrainLogger, ErrorMetric
 from model import train_model
+from configs import ModelConfigs
+
+
+# noinspection PyBroadException
+try:
+    [tf.config.experimental.set_memory_growth(gpu, True) for gpu in tf.config.experimental.list_physical_devices("GPU")]
+except:
+    pass
