05da916ff844450e692cfb90d3087f39dfe4ce8e
Shubham Acharya <shubham190398@gmail.com>
2023-06-08 11:50:50 +0530

    Finishing up train.py

--- train.py
+++ train.py
@@ -73,3 +73,32 @@ model = train_model(
     input_dim=(config.height, config.width, 3),
     output_dim=len(config.vocab)
 )
+
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=config.learning_rate),
+    loss=CTCloss(),
+    metrics=[ErrorMetric(padding_token=len(config.vocab))],
+    run_eagerly=False
+)
+
+model.summary(line_length=110)
+
+os.makedirs(config.model_path, exist_ok=True)
+
+early_stopper = EarlyStopping(monitor="val_CER", patience=50, verbose=1)
+ckpt = ModelCheckpoint(f"{config.model_path}/model.h5", monitor="val_CER", verbose=1, save_best_only=True, mode="min")
+train_logger = TrainLogger(config.model_path)
+tb_callback = TensorBoard(f"{config.model_path}/logs", update_freq=1)
+reduce_LR = ReduceLROnPlateau(monitor="val_CER", factor=0.9, min_delta=1e-10, patience=20, verbose=1, mode="auto")
+model_2_onnx = Model2Onnx(f"{config.model_path}/model.h5")
+
+model.fit(
+    train_data_loader,
+    validation_data=val_data_loader,
+    epochs=config.train_epochs,
+    workers=config.train_workers,
+    callbacks=[early_stopper, ckpt, train_logger, reduce_LR, tb_callback, model_2_onnx]
+)
+
+train_data_loader.to_csv(os.path.join(config.model_path, "train.csv"))
+val_data_loader.to_csv(os.path.join(config.model_path, "val.csv"))
