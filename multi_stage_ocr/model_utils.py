ccdffac0994da70249d18ef3c2af4e38882c593f
Shubham Acharya <shubham190398@gmail.com>
2023-06-12 16:05:26 +0530

    Trying to complicate things disconnects the model. As such, trying out a simple conv block definition

--- multi_stage_ocr/model_utils.py
+++ multi_stage_ocr/model_utils.py
@@ -9,38 +9,11 @@ def conv_block(
         activation='relu',
         padding='same',
         kernel_initializer='he_normal',
-        dropout=False,
-        pooling=True,
-        upsampling=False,
-        merge=None
 ):
-    if not upsampling:
-        x = layers.Conv2D(filter_num, kernel_stride, activation=activation,
-                          padding=padding, kernel_initializer=kernel_initializer)(x)
-        x = layers.Conv2D(filter_num, kernel_stride, activation=activation,
-                          padding=padding, kernel_initializer=kernel_initializer)(x)
-    else:
-        up = layers.Conv2D(filter_num, kernel_stride-1, activation=activation, padding=padding,
-                           kernel_initializer=kernel_initializer)(layers.UpSampling2D(size=(2, 2))(x))
-        x = layers.concatenate([merge, up], axis=3)
-        x = layers.Conv2D(filter_num, kernel_stride, activation=activation,
-                          padding=padding, kernel_initializer=kernel_initializer)(x)
-        x = layers.Conv2D(filter_num, kernel_stride, activation=activation,
-                          padding=padding, kernel_initializer=kernel_initializer)(x)
 
-    y = copy.deepcopy(x)
+    x = layers.Conv2D(filter_num, kernel_stride, activation=activation,
+                      padding=padding, kernel_initializer=kernel_initializer)(x)
+    x = layers.Conv2D(filter_num, kernel_stride, activation=activation,
+                      padding=padding, kernel_initializer=kernel_initializer)(x)
 
-    if dropout:
-        drop = layers.Dropout(0.5)(y)
-
-        if pooling:
-            y = layers.MaxPooling2D(pool_size=(2, 2))(drop)
-
-            return y, drop
-
-        return drop, drop
-
-    if pooling:
-        y = layers.MaxPooling2D(pool_size=(2, 2))(y)
-
-    return y, x
+    return x
