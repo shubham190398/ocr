2899cb400559cd2dd6b1d725e738d7a4c36ced66
Shubham Acharya <shubham190398@gmail.com>
2023-06-12 13:21:17 +0530

    Adding the building blocks for a UNet.

---
+++ multi_stage_ocr/model_utils.py
@@ -0,0 +1,35 @@
+from keras import layers
+
+
+def conv_block(
+        x,
+        filter_num,
+        kernel_stride,
+        activation='relu',
+        padding='same',
+        kernel_initializer='he_normal',
+        dropout=False,
+        pooling=True,
+        upsampling=False,
+):
+    if not upsampling:
+        x = layers.Conv2D(filter_num, kernel_stride, activation=activation,
+                          padding=padding, kernel_initializer=kernel_initializer)(x)
+        x = layers.Conv2D(filter_num, kernel_stride, activation=activation,
+                          padding=padding, kernel_initializer=kernel_initializer)(x)
+    else:
+        up = layers.Conv2D(filter_num, kernel_stride, activation=activation, padding=padding,
+                           kernel_initializer=kernel_initializer)(layers.UpSampling2D(size=(2, 2)))(x)
+        x = layers.concatenate([x, up], axis=3)
+        x = layers.Conv2D(filter_num, kernel_stride, activation=activation,
+                          padding=padding, kernel_initializer=kernel_initializer)(x)
+        x = layers.Conv2D(filter_num, kernel_stride, activation=activation,
+                          padding=padding, kernel_initializer=kernel_initializer)(x)
+
+    if dropout:
+        x = layers.Dropout(dropout=0.5)(x)
+
+    if pooling:
+        x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+
+    return x
