7ad824bc192eda6efba0f9a2121c0644aa768cef
Shubham Acharya <shubham190398@gmail.com>
2023-06-13 12:41:51 +0530

    Writing a batch generator

--- multi_stage_ocr/image_processing.py
+++ multi_stage_ocr/image_processing.py
@@ -21,3 +21,26 @@ def preprocessing(dir_path):
         im[im > 0] = 255
         cv2.imwrite(f"{dir_path}/{image_path}", im)
         print(f"Writing image {image_path}")
+
+
+def batch_generator(dir_path, image_names, batch_size):
+    while True:
+        images = []
+        masks = []
+
+        for i in range(batch_size):
+            im_name = random.choice(image_names)
+            im = cv2.imread(f"{dir_path}/{im_name}.jpg", cv2.IMREAD_GRAYSCALE)
+            _, im = cv2.threshold(im, 150, 255, cv2.THRESH_BINARY_INV)
+            im = cv2.resize(im, (512, 512))
+            im = np.expand_dims(im, axis=-1)
+            im /= 255
+
+            mask = cv2.imread(f"{dir_path}/{im_name}_mask.png", cv2.IMREAD_GRAYSCALE)
+            mask = cv2.resize(mask, (512, 512))
+            mask /= 255
+
+            images.append(im)
+            masks.append(mask)
+
+        yield np.array(images), np.array(masks)
