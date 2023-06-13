931b3a66b82836820bfb387177bfe10540f97c0d
Shubham Acharya <shubham190398@gmail.com>
2023-06-13 11:58:28 +0530

    Adding a function to preprocess the images uniformly to values of 0 or 255.

--- multi_stage_ocr/image_processing.py
+++ multi_stage_ocr/image_processing.py
@@ -12,4 +12,13 @@ def display(img, seg_img):
     plt.show()
 
 
+def preprocessing(dir_path):
+    image_list = os.listdir(dir_path)
+    for image_path in image_list:
+        im = cv2.imread(f"{dir_path}/{image_path}")
+        im[im > 0] = 255
+        cv2.imwrite(f"{dir_path}/{image_path}", im)
+        print(f"Writing image {image_path}")
 
+
+preprocessing("C:\\Users\\Kare4U\\Downloads\\PageSegExtended\\customdataset_extending_pageseg\\PageSeg")
