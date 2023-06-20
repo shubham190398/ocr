48d9e3a5185a0177fda55b785c3d07baf1ebe4c5
Shubham Acharya <shubham190398@gmail.com>
2023-06-20 10:25:39 +0530

    Starting structure for training the extraction model

--- multi_stage_ocr/train_extraction.py
+++ multi_stage_ocr/train_extraction.py
@@ -1,3 +1,23 @@
 import numpy as np
 import cv2
 from text_extraction_utils import encode_labels, preprocess_img, create_vocab
+import os
+import random
+
+
+def train(image_dir, annotation_dir):
+    image_names = os.listdir(image_dir)
+    image_names = [file.split(".")[0] for file in image_names]
+    images = []
+    annotations = []
+
+
+
+def main():
+    image_dir = ""
+    annotation_dir = ""
+    train(image_dir, annotation_dir)
+
+
+if __name__ == "__main__":
+    main()
