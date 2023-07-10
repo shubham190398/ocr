11a1bfb81e513fad1f2f0d652f00bd5f1987885e
Ducco-The-Duck <nexuspower12@gmail.com>
2023-07-10 15:32:30 +0530

    filled in initial bbox data

--- multi_stage_ocr/format_based_data_extraction.py
+++ multi_stage_ocr/format_based_data_extraction.py
@@ -38,24 +38,24 @@ def main():
     dir = os.listdir('dataset/cheque_formats')
     format = {
         'axis': {
-            'name': [],
-            'amount': [],
-            'micr': [],
-            'date': [],
+            'name': [144, 214, 1820, 320],
+            'amount': [1757, 401, 2287, 499],
+            'micr': [422, 926, 1930, 1050],
+            'date': [1801, 87, 2275, 150],
             'data': 'handwritten'
         },
         'california': {
-            'name': [],
-            'amount': [],
-            'micr': [],
-            'date': [],
+            'name': [246, 138, 1662, 432],
+            'amount': [1698, 330, 2141, 446],
+            'micr': [68, 871, 1401, 963],
+            'date': [1439, 186, 1883, 297],
             'data': 'handwritten'
         },
         'canara': {
-            'name': [],
-            'amount': [],
-            'micr': [],
-            'date': [],
+            'name': [200, 230, 1866, 328],
+            'amount': [1841, 407, 2300, 521],
+            'micr': [448, 940, 1920, 1046],
+            'date': [1831, 98, 2300, 155],
             'data': 'handwritten'
         },
         'hbl': {
@@ -66,10 +66,10 @@ def main():
             'data': 'handwritten'
         },
         'icici': {
-            'name': [],
-            'amount': [],
-            'micr': [],
-            'date': [],
+            'name': [310, 216, 2108, 322],
+            'amount': [1854, 396, 2322, 500],
+            'micr': [462, 956, 1872, 1052],
+            'date': [1835, 86, 2316, 149],
             'data': 'handwritten'
         },
         'js': {
@@ -80,10 +80,10 @@ def main():
             'data': 'handwritten'
         },
         'syndicate': {
-            'name': [],
-            'amount': [],
-            'micr': [],
-            'date': [],
+            'name': [184, 210, 1876, 314],
+            'amount': [1821, 399, 2271, 492],
+            'micr': [486, 966, 1862, 1052],
+            'date': [1773, 84, 2301, 150],
             'data': 'handwritten'
         },
     }
@@ -91,7 +91,7 @@ def main():
     # assert dir == list(format.keys())
 
     for file in dir:
-        if not file == 'uncategorised':
+        if file not in ['uncategorised', 'hbl']:
             images_list = os.listdir(f'dataset/formats/{file}')
             for img_file in images_list:
                 func = globals()[f"text_detector_{format[file]['data']}"]
@@ -100,7 +100,15 @@ def main():
                                 format[file]['name'][1]:format[file]['name'][3],
                                 format[file]['name'][0]:format[file]['name'][2]
                             ])
+                cv2.imshow('name', img[
+                                format[file]['name'][1]:format[file]['name'][3],
+                                format[file]['name'][0]:format[file]['name'][2]
+                            ])
                 amount = func(img[
+                                format[file]['amount'][1]:format[file]['amount'][3],
+                                format[file]['amount'][0]:format[file]['amount'][2]
+                            ])
+                cv2.imshow('amount', img[
                                 format[file]['amount'][1]:format[file]['amount'][3],
                                 format[file]['amount'][0]:format[file]['amount'][2]
                             ])
@@ -108,24 +116,33 @@ def main():
                                 format[file]['micr'][1]:format[file]['micr'][3],
                                 format[file]['micr'][0]:format[file]['micr'][2]
                             ])
+                cv2.imshow('micr', img[
+                                format[file]['micr'][1]:format[file]['micr'][3],
+                                format[file]['micr'][0]:format[file]['micr'][2]
+                            ])
                 date = func(img[
                                 format[file]['date'][1]:format[file]['date'][3],
                                 format[file]['date'][0]:format[file]['date'][2]
                             ])
-                f = open(f'results/format_results/{img_file.split(".")[0]}.txt', 'w')
-                micr_codes = []
-                for string in re.split('A|B|C|D|A |B |C |D ', micr):
-                    if string != '':
-                        full = ''
-                        for sub in string.split():
-                            full += sub
-                        micr_codes.append(full)
-                f.write('Name: ' + name + '\n' +
-                        'Amount: ' + amount + '\n' +
-                        'Date: ' + date + '\n' +
-                        'Cheque Number: ' + micr_codes[0] + '\n' +
-                        'Full MICR: ' + str([f'{micr_codes[i]} ' for i in range(len(micr_codes)-1)]) + micr_codes[-1])
-                f.close()
+                cv2.imshow('date', img[
+                                format[file]['date'][1]:format[file]['date'][3],
+                                format[file]['date'][0]:format[file]['date'][2]
+                            ])
+                cv2.waitKey(0)
+                # f = open(f'results/format_results/{img_file.split(".")[0]}.txt', 'w')
+                # micr_codes = []
+                # for string in re.split('A|B|C|D|A |B |C |D ', micr):
+                #     if string != '':
+                #         full = ''
+                #         for sub in string.split():
+                #             full += sub
+                #         micr_codes.append(full)
+                # f.write('Name: ' + name + '\n' +
+                #         'Amount: ' + amount + '\n' +
+                #         'Date: ' + date + '\n' +
+                #         'Cheque Number: ' + micr_codes[0] + '\n' +
+                #         'Full MICR: ' + str([f'{micr_codes[i]} ' for i in range(len(micr_codes)-1)]) + micr_codes[-1])
+                # f.close()
 
 
 main()
