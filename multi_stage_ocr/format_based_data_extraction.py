115933ec8930315c5875d6d40b7c23102f71d4bc
Ducco-The-Duck <nexuspower12@gmail.com>
2023-07-10 14:05:08 +0530

    changes to script

--- multi_stage_ocr/format_based_data_extraction.py
+++ multi_stage_ocr/format_based_data_extraction.py
@@ -35,61 +35,97 @@ def text_detector_handwritten(image):
 
 
 def main():
-    dir = os.listdir('dataset/formats')
+    dir = os.listdir('dataset/cheque_formats')
     format = {
-        'format1': {
+        'axis': {
             'name': [],
             'amount': [],
             'micr': [],
             'date': [],
             'data': 'handwritten'
         },
-        'format2': {
+        'california': {
             'name': [],
             'amount': [],
             'micr': [],
             'date': [],
-            'data': 'printed'
-        }
+            'data': 'handwritten'
+        },
+        'canara': {
+            'name': [],
+            'amount': [],
+            'micr': [],
+            'date': [],
+            'data': 'handwritten'
+        },
+        'hbl': {
+            'name': [],
+            'amount': [],
+            'micr': [],
+            'date': [],
+            'data': 'handwritten'
+        },
+        'icici': {
+            'name': [],
+            'amount': [],
+            'micr': [],
+            'date': [],
+            'data': 'handwritten'
+        },
+        'js': {
+            'name': [],
+            'amount': [],
+            'micr': [],
+            'date': [],
+            'data': 'handwritten'
+        },
+        'syndicate': {
+            'name': [],
+            'amount': [],
+            'micr': [],
+            'date': [],
+            'data': 'handwritten'
+        },
     }
 
     # assert dir == list(format.keys())
 
     for file in dir:
-        images_list = os.listdir(f'dataset/formats/{file}')
-        for img_file in images_list:
-            func = globals()[f"text_detector_{format[file]['data']}"]
-            img = cv2.imread(f'dataset/formats/{file}/{img_file}')
-            name = func(img[
-                            format[file]['name'][1]:format[file]['name'][3],
-                            format[file]['name'][0]:format[file]['name'][2]
-                        ])
-            amount = func(img[
-                            format[file]['amount'][1]:format[file]['amount'][3],
-                            format[file]['amount'][0]:format[file]['amount'][2]
-                        ])
-            micr = text_detector_MICR(img[
-                            format[file]['micr'][1]:format[file]['micr'][3],
-                            format[file]['micr'][0]:format[file]['micr'][2]
-                        ])
-            date = func(img[
-                            format[file]['date'][1]:format[file]['date'][3],
-                            format[file]['date'][0]:format[file]['date'][2]
-                        ])
-            f = open(f'results/format_results/{img_file.split(".")[0]}.txt', 'w')
-            micr_codes = []
-            for string in re.split('A|B|C|D|A |B |C |D ', micr):
-                if string != '':
-                    full = ''
-                    for sub in string.split():
-                        full += sub
-                    micr_codes.append(full)
-            f.write('Name: ' + name + '\n' +
-                    'Amount: ' + amount + '\n' +
-                    'Date: ' + date + '\n' +
-                    'Cheque Number: ' + micr_codes[0] + '\n' +
-                    'Full MICR: ' + str([f'{micr_codes[i]} ' for i in range(len(micr_codes)-1)]) + micr_codes[-1])
-            f.close()
+        if not file == 'uncategorised':
+            images_list = os.listdir(f'dataset/formats/{file}')
+            for img_file in images_list:
+                func = globals()[f"text_detector_{format[file]['data']}"]
+                img = cv2.imread(f'dataset/formats/{file}/{img_file}')
+                name = func(img[
+                                format[file]['name'][1]:format[file]['name'][3],
+                                format[file]['name'][0]:format[file]['name'][2]
+                            ])
+                amount = func(img[
+                                format[file]['amount'][1]:format[file]['amount'][3],
+                                format[file]['amount'][0]:format[file]['amount'][2]
+                            ])
+                micr = text_detector_MICR(img[
+                                format[file]['micr'][1]:format[file]['micr'][3],
+                                format[file]['micr'][0]:format[file]['micr'][2]
+                            ])
+                date = func(img[
+                                format[file]['date'][1]:format[file]['date'][3],
+                                format[file]['date'][0]:format[file]['date'][2]
+                            ])
+                f = open(f'results/format_results/{img_file.split(".")[0]}.txt', 'w')
+                micr_codes = []
+                for string in re.split('A|B|C|D|A |B |C |D ', micr):
+                    if string != '':
+                        full = ''
+                        for sub in string.split():
+                            full += sub
+                        micr_codes.append(full)
+                f.write('Name: ' + name + '\n' +
+                        'Amount: ' + amount + '\n' +
+                        'Date: ' + date + '\n' +
+                        'Cheque Number: ' + micr_codes[0] + '\n' +
+                        'Full MICR: ' + str([f'{micr_codes[i]} ' for i in range(len(micr_codes)-1)]) + micr_codes[-1])
+                f.close()
 
 
 main()
