7e655b1b6b308e1c51e099f31a55138258a19a81
mshibashish <shibashish.mahapatra@kare4u.in>
2023-07-16 01:13:03 +0530

    bad_images results

---
+++ multi_stage_ocr/bad_images_remits.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
+import numpy as np
+
+def text_detector_printed(image):
+    processor = TrOCRProcessor.from_pretrained("microsoft/trocr-large-printed")
+    model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-large-printed")
+    pixel_values = processor(image, return_tensors="pt").pixel_values
+    generated_ids = model.generate(pixel_values)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    print(generated_text)
+    return generated_text
+
+
+def main():
+    dir = os.listdir('dataset/bad_images')
+    f = open('results/bad_images_remits_text.txt', 'w')
+    for file in dir:
+        path = 'dataset/bad_images/' + file
+        img = cv2.imread(path)
+        text = text_detector_printed(img)
+        f.write(file + ', ' + text + '\n')
+    f.close()
+
+main()
