269bd2b25a97a4a809d0e07659e221f053d1671e
mshibashish <shibashish.mahapatra@kare4u.in>
2023-07-17 11:11:35 +0530

    bad_images results including some micr images

--- multi_stage_ocr/bad_images_remits.py
+++ multi_stage_ocr/bad_images_remits.py
@@ -12,14 +12,23 @@ def text_detector_printed(image):
     print(generated_text)
     return generated_text
 
+def text_detector_MICR(image):
+    processor = TrOCRProcessor.from_pretrained("microsoft/trocr-large-printed")
+    model = VisionEncoderDecoderModel.from_pretrained("Apocalypse-19/trocr-MICR")
+    pixel_values = processor(image, return_tensors="pt").pixel_values
+    generated_ids = model.generate(pixel_values)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    print(generated_text)
+    return generated_text
+
 
 def main():
-    dir = os.listdir('dataset/bad_images')
-    f = open('results/bad_images_remits_text.txt', 'w')
+    dir = os.listdir('dataset/bad_images/micr')
+    f = open('results/bad_images_micr.txt', 'w')
     for file in dir:
-        path = 'dataset/bad_images/' + file
+        path = 'dataset/bad_images/micr/' + file
         img = cv2.imread(path)
-        text = text_detector_printed(img)
+        text = text_detector_MICR(img)
         f.write(file + ', ' + text + '\n')
     f.close()
 
