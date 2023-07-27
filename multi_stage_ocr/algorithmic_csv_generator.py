9209f820a1a39811f822d796c7590c8a73eb1d83
Shubham Acharya <shubham190398@gmail.com>
2023-07-27 11:35:40 +0530

    Crop function is verified. Moving on to row check function

--- multi_stage_ocr/algorithmic_csv_generator.py
+++ multi_stage_ocr/algorithmic_csv_generator.py
@@ -1,15 +1,20 @@
 import easyocr
 import cv2
-from typing import Any
+from typing import Any, List, Tuple, Dict
 
 
-def crops(image_path: str, reader: Any) -> None:
+def crops(image_path: str, reader: Any) -> List[Tuple]:
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     results = reader.readtext(image)
-    print(results)
+    return results
 
 
-def main():
+def row_check(results: List[Tuple]) -> Dict:
+    row_dict = {}
+    return row_dict
+
+
+def main() -> None:
     image_path = "dataset/consolidated_remittances/3_B.png"
     reader = easyocr.Reader(['en'])
     crops(image_path, reader)
