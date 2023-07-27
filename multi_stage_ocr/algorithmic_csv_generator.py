7e632196d42f83a9f0b281a237cdac90e8645b19
Shubham Acharya <shubham190398@gmail.com>
2023-07-27 11:59:55 +0530

    Function to separate the detection into different rows created

--- multi_stage_ocr/algorithmic_csv_generator.py
+++ multi_stage_ocr/algorithmic_csv_generator.py
@@ -11,13 +11,29 @@ def crops(image_path: str, reader: Any) -> List[Tuple]:
 
 def row_check(results: List[Tuple]) -> Dict:
     row_dict = {}
+    last_row_y_position = -100
+    last_row = 0
+
+    for detection in results:
+        bbox = detection[0]
+        x1, y1 = bbox[0]
+        if y1 - last_row_y_position > 30:
+            last_row += 1
+            last_row_y_position = y1
+            row_dict[last_row] = []
+            row_dict[last_row].append([bbox[0], bbox[2]])
+        else:
+            row_dict[last_row].append([bbox[0], bbox[2]])
+
     return row_dict
 
 
 def main() -> None:
     image_path = "dataset/consolidated_remittances/3_B.png"
     reader = easyocr.Reader(['en'])
-    crops(image_path, reader)
+    results = crops(image_path, reader)
+    rows = row_check(results)
+    print(rows)
 
 
 if __name__ == '__main__':
