82b4432dfe22f291bf0c73665ec4878a3b8e3d3a
Shubham Acharya <shubham190398@gmail.com>
2023-07-27 13:11:27 +0530

    Adding function to get crops

--- multi_stage_ocr/algorithmic_csv_generator.py
+++ multi_stage_ocr/algorithmic_csv_generator.py
@@ -1,9 +1,11 @@
 import easyocr
 import cv2
 from typing import Any, List, Tuple, Dict
+import numpy as np
+from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 
 
-def crops(image_path: str, reader: Any) -> List[Tuple]:
+def recognize_text(image_path: str, reader: Any) -> List[Tuple]:
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     results = reader.readtext(image)
     return results
@@ -28,12 +30,26 @@ def row_check(results: List[Tuple]) -> Dict:
     return row_dict
 
 
+def get_crops(row_dict: Dict, image_path: str) -> Dict:
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    crop_dict = {}
+    for key, value in row_dict.items():
+        crop_dict[key] = []
+        for v in value:
+            x1, y1 = v[0]
+            x2, y2 = v[1]
+            cropped_img = image[y1:y2, x1:x2]
+            crop_dict[key].append(cropped_img)
+
+    return crop_dict
+
+
 def main() -> None:
     image_path = "dataset/consolidated_remittances/3_B.png"
     reader = easyocr.Reader(['en'])
-    results = crops(image_path, reader)
+    results = recognize_text(image_path, reader)
     rows = row_check(results)
-    print(rows)
+    crops = get_crops(rows, image_path)
 
 
 if __name__ == '__main__':
