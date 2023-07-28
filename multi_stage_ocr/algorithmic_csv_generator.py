b289d29f3cff621d5f890b19a8f5b249cde9bd18
Ducco-The-Duck <nexuspower12@gmail.com>
2023-07-28 13:23:24 +0530

    bad_images. 5 and less are easy + trocr. 6 and above are easy only

--- multi_stage_ocr/algorithmic_csv_generator.py
+++ multi_stage_ocr/algorithmic_csv_generator.py
@@ -1,3 +1,5 @@
+import time
+
 import easyocr
 import cv2
 from typing import Any, List, Tuple, Dict
@@ -67,6 +69,31 @@ def get_csv(texts: Dict, name: str) -> None:
     # text_file.to_csv("results/full_extraction/BA_1.csv", index=None)
 
 
+def get_text_from_EasyOCR(image_path: str, reader: Any):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    results = reader.readtext(image)
+    row_dict = {}
+    text_dict = {}
+    last_row_y_position = -100
+    last_row = 0
+
+    for detection in results:
+        bbox = detection[0]
+        x1, y1 = bbox[0]
+        if y1 - last_row_y_position > 5:
+            last_row += 1
+            last_row_y_position = y1
+            row_dict[last_row] = []
+            row_dict[last_row].append([[int(bbox[0][0]), int(bbox[0][1])], [int(bbox[2][0]), int(bbox[2][1])]])
+            text_dict[last_row] = []
+            text_dict[last_row].append(detection[1])
+        else:
+            row_dict[last_row].append([[int(bbox[0][0]), int(bbox[0][1])], [int(bbox[2][0]), int(bbox[2][1])]])
+            text_dict[last_row].append(detection[1])
+
+    return text_dict
+
+
 def get_A_images_from_cons_rem() -> List:
     print('get_img func entered')
     img_dir = os.listdir('dataset/consolidated_remittances')
@@ -116,13 +143,25 @@ def get_bad_images_from_cons_rem() -> List:
 def main() -> None:
     print('main entered')
     reader = easyocr.Reader(['en'])
+    f = open('results/full_extraction/times_for_bad_images.txt', 'w')
+
     for file in get_bad_images_from_cons_rem():
         print(file)
-        image_path = "dataset/bad_images/invoices/" + file
-        results = recognize_text(image_path, reader)
-        rows = row_check(results)
-        texts = get_text(rows, image_path)
-        get_csv(texts, file.split('.')[0])
+        t = time.time()
+        if int(file.split('.')[0].split('_')[1]) < 6:
+            image_path = "dataset/bad_images/invoices/" + file
+            results = recognize_text(image_path, reader)
+            rows = row_check(results)
+            texts = get_text(rows, image_path)
+            get_csv(texts, file.split('.')[0])
+        else:
+            image_path = "dataset/bad_images/invoices/" + file
+            texts = get_text_from_EasyOCR(image_path, reader)
+            get_csv(texts, file.split('.')[0])
+        print('Time taken for' + file + ' is ' + str(time.time() - t))
+        f.write('Time taken for' + file + ' is ' + str(time.time() - t) + '\n')
+    f.close()
+
 
 
 if __name__ == '__main__':
