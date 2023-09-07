f38d92110695ca47ca93467369bf73877798b20d
Ducco-The-Duck <nexuspower12@gmail.com>
2023-09-07 09:15:53 +0530

    updated API_Checkmatic extraction for PaddleOCR

--- multi_stage_ocr/API_CheckMatic_script/extraction.py
+++ multi_stage_ocr/API_CheckMatic_script/extraction.py
@@ -1,10 +1,10 @@
 import csv
 import time
-
+from paddleocr import PaddleOCR
 import cv2
 import torch.cuda
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
-from easyocr import Reader
+import numpy as np
 from pypdfium2 import PdfDocument
 
 
@@ -18,41 +18,66 @@ def detection(image, model, processor):
     return text
 
 
-def get_text_region(image, reader):
+def get_text_region(image, ocr):
     img = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    results = reader.readtext(img)
+    results = ocr.ocr(img, cls=True)
     return results
 
 
-def get_rows(results, row_separation):
+def reformat_boxes_to_be_usable_with_row_dict_func(result):
+    """
+    given the paddleocr result of an image, this function will return a list of bboxes
+    ordered according to their y1 value (i.e. top-left corner y-value)
+    :param result: paddle ocr result for an image
+    :return: ordered list of bboxes
+    """
+    boxes = [(int(line[0][0][1]), line[0]) for line in result]
+
+    dtype = [('height', int), ('list', list)]
+    bboxes_with_height = np.array(boxes, dtype)
+    bboxes_sorted = np.sort(bboxes_with_height, order='height')
+    bboxes = []
+    for _, bbox in bboxes_sorted:
+        x1 = int(bbox[0][0])
+        y1 = int(bbox[0][1])
+        x2 = int(bbox[2][0])
+        y2 = int(bbox[2][1])
+        bboxes.append([(x1, y1), (x2, y2)])
+
+    return bboxes
+
+
+def reformat_into_rows(bboxes, row_sep):
+    """
+    takes a list of bboxes which are ordered according to their y-value and returns a
+    row_dict, where bboxes are ordered according to their x-value within each row
+    :param bboxes: ordered list of bboxes (according to y-value)
+    :param row_sep: distance for separating rows
+    :return: ordered row dict of bboxes
+    """
     row_dict = {}
-    row_count = 0
-    last_row_position = -100
+    last_row_y_position = -100
+    last_row = 0
 
-    for result in results:
-        bbox = result[0]
-        y1 = bbox[0][1]
+    for box in bboxes:
+        x1, y1 = box[0]
+        x2, y2 = box[1]
+        if y1 - last_row_y_position > row_sep:
+            last_row += 1
+            last_row_y_position = y1
+            row_dict[last_row] = []
+        row_dict[last_row].append((x1, [[x1, y1], [x2, y2]]))
 
-        if isinstance(y1, float):
-            x1, y1, x2, y2 = int(bbox[0][0]), int(bbox[0][1]), int(bbox[2][0]), int(bbox[2][1])
+    for key, value in row_dict.items():
+        dtype = [('x', int), ('list', list)]
+        values_to_sort = np.array(value, dtype)
+        values_sorted = np.sort(values_to_sort, order='x')
+        new_value = []
+        for _, bbox in values_sorted:
+            new_value.append(bbox)
+        row_dict[key] = new_value
 
-            for key in row_dict.keys():
-                if y1 - row_dict[key][0][0][1] <= row_separation:
-                    for i, bbox2 in enumerate(row_dict[key]):
-                        if x1 < bbox2[0][0]:
-                            row_dict[key].insert(i, [[x1, y1], [x2, y2]])
-                            break
-                    break
-
-        else:
-            if y1 - last_row_position > row_separation:
-                row_count += 1
-                last_row_position = y1
-                row_dict[row_count] = []
-
-            x1, y1, x2, y2 = int(bbox[0][0]), int(bbox[0][1]), int(bbox[2][0]), int(bbox[2][1])
-            row_dict[row_count].append([[x1, y1], [x2, y2]])
-
+    print(row_dict)
     return row_dict
 
 
@@ -81,28 +106,28 @@ def write_csv(texts):
             writer.writerow(value)
 
 
-def invoice_transcribe(model, processor, reader, invoice_file):
-    results = get_text_region(invoice_file, reader)
-    rows = get_rows(results, 7)
+def invoice_transcribe(model, processor, ocr, invoice_file):
+    results = get_text_region(invoice_file, ocr)
+    rows = reformat_into_rows(reformat_boxes_to_be_usable_with_row_dict_func(results[0]), 7)
     texts = get_text(rows, model, processor, invoice_file, 7)
     write_csv(texts)
 
 
-def transcribe(model, processor, reader, pdf_file_path):
+def transcribe(model, processor, ocr, pdf_file_path):
     pdf_file = PdfDocument(pdf_file_path)
     invoice_file = pdf_file[len(pdf_file) - 1]
     invoice_file = invoice_file.render(scale=2).to_numpy()
-    invoice_transcribe(model, processor, reader, invoice_file)
+    invoice_transcribe(model, processor, ocr, invoice_file)
 
 
 def main(pdf_file_path):
-    reader = Reader(['en'])
+    ocr = PaddleOCR(use_angle_cls=True, lang='en')
 
     processor = TrOCRProcessor.from_pretrained("microsoft/trocr-large-printed")
     model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-large-printed")
     model.to(DEVICE)
 
-    transcribe(model, processor, reader, pdf_file_path)
+    transcribe(model, processor, ocr, pdf_file_path)
 
 
 path = "../dataset/bad_img_pdfs/Bad Image 2.pdf"
